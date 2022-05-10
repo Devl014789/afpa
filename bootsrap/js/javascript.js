89d62cd9596af244f66aeb03ce1edee79fd49e4a
Devl014789 <devl014789@gmail.com>
2022-05-10 16:37:32 +0200

    new update jquery

---
+++ bootsrap/js/javascript.js
@@ -0,0 +1,20 @@
+$( function () {
+    $(".login").click(function() {
+        valid = true;
+        if($("#email").val == "Emailgit ") {
+            $("#email").css("border-color", "red");
+            $("#email").addClass("redplace");
+            valid = false;
+        } else {
+            $("#email").css("border-color", "green");
+        }
+        if($("#pass").val != "") {
+            $("#pass").css("border-color", "red");
+            $("#pass").addClass("redplace");
+            valid = false;
+        } else {
+            $("#pass").css("border-color", "green");
+        }
+        return valid
+    });
+});
