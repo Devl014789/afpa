1f42878a1395b82f7dbfefc2baaf76bea9fa7f50
Devl014789 <Devl014789@gmail.com>
2022-05-18 19:11:09 +0200

    final version

--- calacul/js/script.js
+++ calacul/js/script.js
@@ -1,4 +1,5 @@
 let input = '';
+
 function ShowValue(btn) {
     input = input + btn.value;
     document.getElementById('operation').innerText = input;
@@ -7,7 +8,7 @@ function ShowValue(btn) {
 
 function ckMath() {
     let tmp = eval(input);
-    if (tmp != SyntaxError) {
+    if (tmp != undefined) {
         document.getElementById('resultat').innerText = tmp;
     } else {
         document.getElementById('resultat').innertext = tmp = 'Error';
@@ -20,3 +21,25 @@ function Reset() {
     input = '';
     document.getElementById('resultat').innerText = input
 };
+
+function previousRemove() {
+ let leninput = input.length;
+ input = input.slice(0, leninput - 1);
+ document.getElementById('operation').innerText = input;
+}
+
+function calculRacine() {
+ input = Math.sqrt(input)
+ document.getElementById('resultat').innerText = input;
+ input = ''
+}
+
+function calculPi() {
+ if(input == '0') {
+     input = Math.PI + input;
+ } else {
+     input = input + Math.PI
+ }
+ document.getElementById('operation').innerText = input;
+
+}
